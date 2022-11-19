361457834ef41f579df5f33d7387a1007cc45a7b
Vichenry2002 <victor.henry@mail.mcgill.ca>
2022-11-19 14:01:33 -0500

    nice

---
+++ WorldCupBet/src/main/java/com/example/worldcupbet/WorldCupBetApplication.java
@@ -0,0 +1,13 @@
+package com.example.worldcupbet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldCupBetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorldCupBetApplication.class, args);
+    }
+
+}
---
+++ WorldCupBet/src/test/java/com/example/worldcupbet/WorldCupBetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.worldcupbet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldCupBetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
